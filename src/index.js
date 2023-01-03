9569c1ee647b9518f3e283d5cd1376597c2684ac
Just-Ven <alnurbaisar@gmail.com>
2023-01-03 16:42:16 +0600

    git pu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        const str = String(n)
+        const arr = Array.from(str)
+        arr.reverse()
+        const num = arr.join('')
+        return +num
+    }
+    else {
+        const str = String(n)
+        const arr = Array.from(str)
+        arr.reverse().pop()
+        const num = arr.join('')
+        return +num
+    }
 }
